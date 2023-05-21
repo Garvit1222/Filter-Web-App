4e9260baf430a0970c5c9bf92f5da43aedf38b21
Garvit1222 <121927843+Garvit1222@users.noreply.github.com>
2023-05-21 09:31:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+function preload()
+{
+
+}
+
+
+
+function setup()
+{
+    canvas = createCanvas(450,300);
+    canvas.center();
+}
+
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    
+}
